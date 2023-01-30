d72b10cce60ab8da02537ec22104d29686ae162f
Tobias Aagaard <Tobias909@live.dk>
2023-01-30 09:42:20 +0100

    Styled components
    Container

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import { ContainerStyle } from "./Components/Styled/Container.style";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ContainerStyle bg="red">fnsajfnsja</ContainerStyle>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Styled/Container.style.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+export const ContainerStyle = styled.section`
+    border: solid 1px #000;
+    background-color ${(props) => props.bg}
+`;
