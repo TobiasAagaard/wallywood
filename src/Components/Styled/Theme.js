286c91a7cefde74ee77ff763bc5f5bda04998b15
Tobias Aagaard <Tobias909@live.dk>
2023-01-30 11:07:54 +0100

    theme

---
+++ src/Components/Styled/Theme.js
@@ -0,0 +1,9 @@
+export const theme = {
+  colors: {
+    primary: "#5C1F06",
+    Secondary: "D97852",
+    light: "#fff",
+    dark: "#000",
+  },
+  fonts: ["Titillium Web", "Open Sans"],
+};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./Components/Styled/Theme";
+import { GlobalStyle } from "./Components/Styled/Global.style";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
