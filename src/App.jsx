e98d5dba2c6e8bffe5b0e6bfe5b79a5df29f5a0f
Tobias Aagaard <Tobias909@live.dk>
2023-01-30 09:46:12 +0100

    global style

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { GlobalStyle } from "./Components/Styled/Global.style";
 import { ContainerStyle } from "./Components/Styled/Container.style";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <ContainerStyle bg="red">fnsajfnsja</ContainerStyle>
     </div>
   );
